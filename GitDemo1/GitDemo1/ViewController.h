6d8870b6544a20debd2cfe2f0f20879a0d568426
students <students@jzwvip.local>
2016-01-18 16:41:53 -0800

    12121

---
+++ GitDemo1/GitDemo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo1
+//
+//  Created by students on 16/1/18.
+//  Copyright © 2016年 students. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitDemo1/GitDemo1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitDemo1
+//
+//  Created by students on 16/1/18.
+//  Copyright © 2016年 students. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
